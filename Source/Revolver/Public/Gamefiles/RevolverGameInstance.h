99bb64dd6e6fa13903a714ef9f6e67ab3f6feb06
Kieran T <kieran123416@gmail.com>
2026-02-04 15:34:08 +0000

    Initial Project Setups

---
+++ Source/Revolver/Public/Gamefiles/RevolverGameInstance.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "RevolverGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REVOLVER_API URevolverGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+};
--- Source/Revolver/Revolver.Build.cs
+++ Source/Revolver/Revolver.Build.cs
@@ -8,9 +8,18 @@ public class Revolver : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput", 
+			"CommonUI",
+			"CommonInput"
+		});
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
