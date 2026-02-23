7a603eda8a799f4894a7f439483935453c66a85d
Kieran T <kieran123416@gmail.com>
2026-02-23 00:36:31 +0000

    started implementing the base weapon functionality to be used with the ability component

---
+++ Source/Revolver/Private/Weapons/WeaponBase.cpp
@@ -0,0 +1,75 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapons/WeaponBase.h"
+// engine
+#include "AbilitySystemComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+
+// Sets default values
+AWeaponBase::AWeaponBase()
+{
+	WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon Mesh");
+	WeaponMesh->SetupAttachment(RootComponent);
+	
+	TraceStart = CreateDefaultSubobject<USceneComponent>("Trace Start");
+	TraceStart->SetupAttachment(GetRootComponent());
+	
+	TraceEnd = CreateDefaultSubobject<USceneComponent>("Trace End");
+	TraceEnd->SetupAttachment(GetRootComponent());
+}
+
+
+
+// Called when the game starts or when spawned
+void AWeaponBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AWeaponBase::HitScan()
+{
+	FVector StartLocation = TraceStart->GetComponentLocation(); 
+	FVector EndLocation = TraceEnd->GetComponentLocation();
+	
+	TArray<AActor*> IgnoreActors;
+	IgnoreActors.Add(GetInstigator()); 
+	
+	TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypes; 
+	ObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECC_Pawn));
+	
+	TArray<FHitResult> HitResults;
+	
+	bool bHit = UKismetSystemLibrary::SphereTraceMultiForObjects(GetWorld(), StartLocation, EndLocation,TraceRadius,ObjectTypes,
+	false,IgnoreActors,EDrawDebugTrace::ForDuration,HitResults,true); 
+	for (FHitResult Hit : HitResults)
+	{
+		if (!Hit.GetActor())continue; 
+		if (UAbilitySystemComponent* ASC =  Hit.GetActor()->GetComponentByClass<UAbilitySystemComponent>())
+		{
+			if (!HitActors.Contains(Hit.GetActor()))
+			{
+				HitActors.AddUnique(Hit.GetActor());
+				ASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
+			}
+		}
+	}
+}
+
+// the activation of the montage it will play an event and the event will call this from the weapon reference. 
+void AWeaponBase::HitScanStart(const FGameplayEffectSpecHandle& InEffectSpecHandle)
+{
+	EffectSpecHandle = InEffectSpecHandle;
+	HitActors.Empty(); 
+	GetWorldTimerManager().SetTimer(HitScanTimer,this, &AWeaponBase::HitScan,TraceTimer,true); 
+}
+
+// same idea but with it waiting for the ability to end 
+void AWeaponBase::HitScanEnd()
+{
+	GetWorldTimerManager().ClearTimer(HitScanTimer); 
+}
+
+
---
+++ Source/Revolver/Public/Weapons/WeaponBase.h
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GameplayEffectTypes.h"
+#include "WeaponBase.generated.h"
+
+UCLASS()
+class REVOLVER_API AWeaponBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AWeaponBase();
+
+	UFUNCTION()
+	void HitScanStart(const FGameplayEffectSpecHandle& InEffectSpecHandle);
+	
+	UFUNCTION()
+	void HitScanEnd();
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	void HitScan(); 
+private: 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+	TObjectPtr<USkeletalMeshComponent> WeaponMesh; 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+	TObjectPtr<USceneComponent> TraceStart; 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+	TObjectPtr<USceneComponent> TraceEnd;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Hit Scan", meta = (AllowPrivateAccess = true))
+	float TraceRadius;
+	UPROPERTY(EditDefaultsOnly, BLueprintReadOnly, Category = "Hit Scan", meta = (AllowPrivateAccess = true))
+	float TraceTimer; 
+	
+	FGameplayEffectSpecHandle EffectSpecHandle; 
+	
+	FTimerHandle HitScanTimer;
+	
+	UPROPERTY()
+	TArray<AActor*> HitActors;
+	
+};
