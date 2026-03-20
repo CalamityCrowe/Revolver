8fe51cde45d0fcde858c32a2079f0acc4c4ea8a8
Kieran T <kieran123416@gmail.com>
2026-03-20 01:51:40 +0000

    testing a tag system in blueprints to handle cameras instead

---
+++ Source/Revolver/Private/Components/AbilityCameraManagerComponent.cpp
@@ -0,0 +1,71 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/AbilityCameraManagerComponent.h"
+
+#include "Abilities/Async/AbilityAsync_WaitGameplayTag.h"
+#include "Characters/Player/RevolverPlayerCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+
+UAbilityCameraManagerComponent::UAbilityCameraManagerComponent()
+{
+
+	PrimaryComponentTick.bCanEverTick = false;
+
+}
+
+void UAbilityCameraManagerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void UAbilityCameraManagerComponent::IntializeCameraSetup()
+{
+	PlayerRef = Cast<ARevolverPlayerCharacter>(GetOwner());
+	if (PlayerRef)
+	{
+		CameraBoomRef = PlayerRef->GetCameraBoom();
+	}
+	if (!IsValid(CameraBoomRef))
+	{
+		UE_LOG(LogTemp,Error,TEXT("CameraBoomRef is invalid"));
+		return;
+	}
+	
+UAbilityAsync_WaitGameplayTagAdded* Ability1TagAdded = UAbilityAsync_WaitGameplayTagAdded::WaitGameplayTagAddToActor
+	(PlayerRef,FGameplayTag::RequestGameplayTag("GameplayEffect.Character.Ability1"),false);
+	Ability1TagAdded->Added.AddDynamic(this,&UAbilityCameraManagerComponent::OnAbility1CameraLerp); 
+	
+	UAbilityAsync_WaitGameplayTagRemoved* Ability1TagRemoved = UAbilityAsync_WaitGameplayTagRemoved::WaitGameplayTagRemoveFromActor
+	(PlayerRef, FGameplayTag::RequestGameplayTag("GameplayEffect.Character.Ability1"),false); 
+	Ability1TagRemoved->Removed.AddDynamic(this,&UAbilityCameraManagerComponent::AbilityFinished);
+}
+
+void UAbilityCameraManagerComponent::OnAbility1CameraLerp()
+{
+	
+}
+
+void UAbilityCameraManagerComponent::AbilityFinished()
+{
+	StartArmLength = CameraBoomRef->TargetArmLength; 
+	StartSocket = CameraBoomRef->SocketOffset; 
+	
+	TargetArmLength = DefaultArmLength;
+	TargetSocket = DefaultSocket;
+	
+	if (GetWorld()->GetTimerManager().IsTimerActive(CameraLerpTimer))
+	{
+		GetWorld()->GetTimerManager().ClearTimer(CameraLerpTimer);
+	}
+	GetWorld()->GetTimerManager().SetTimer(CameraLerpTimer,this,&UAbilityCameraManagerComponent::LerpCamera, 0.1f, true);
+}
+
+void UAbilityCameraManagerComponent::LerpCamera()
+{
+	// I am needing to set this up
+}
+
+
+
---
+++ Source/Revolver/Public/Components/AbilityCameraManagerComponent.h
@@ -0,0 +1,53 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "AbilityCameraManagerComponent.generated.h"
+
+
+class ARevolverPlayerCharacter;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class REVOLVER_API UAbilityCameraManagerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UAbilityCameraManagerComponent();
+	
+	virtual void BeginPlay() override;
+	
+	void IntializeCameraSetup(); 
+
+protected: 
+	
+	UFUNCTION()
+	void OnAbility1CameraLerp();
+	
+	UFUNCTION()
+	void AbilityFinished(); 
+	
+private: 
+	
+	void LerpCamera();
+	
+	UPROPERTY()
+	USpringArmComponent* CameraBoomRef;
+	UPROPERTY()
+	ARevolverPlayerCharacter* PlayerRef;
+	
+	FVector StartSocket;
+	float StartArmLength;
+	
+	FVector TargetSocket;
+	float TargetArmLength;
+	
+	FVector DefaultSocket; 
+	float DefaultArmLength;
+	
+	FTimerHandle CameraLerpTimer; 
+	
+};
