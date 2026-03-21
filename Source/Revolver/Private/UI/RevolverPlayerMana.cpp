8d058d97a5e8800f2c33eee96a6fd151b3cbf99f
Kieran T <kieran123416@gmail.com>
2026-03-21 15:38:41 +0000

    UI updated to show ability cost and how much energy the player has

---
+++ Source/Revolver/Private/UI/RevolverPlayerMana.cpp
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/RevolverPlayerMana.h"
+#include "AbilitySystemComponent.h"
+#include "Components/ProgressBar.h"
+#include "Components/TextBlock.h"
+#include "GAS/Attributes/RevolverAttributeSet.h"
+#include "Player/RevolverPlayerState.h"
+
+URevolverPlayerMana::URevolverPlayerMana(const FObjectInitializer& ObjectInitializer):Super(ObjectInitializer)  
+{
+}
+
+void URevolverPlayerMana::NativeConstruct()
+{
+	Super::NativeConstruct();
+	if (ARevolverPlayerState* PS = GetOwningPlayerState<ARevolverPlayerState>())
+	{
+		if (UAbilitySystemComponent* ASC=  PS->GetAbilitySystemComponent())
+		{
+			GEngine->AddOnScreenDebugMessage(-1,5,FColor::Yellow,"MANA"); 
+			Mana = ASC->GetNumericAttribute(URevolverAttributeSet::GetManaAttribute()); 
+			MaxMana = ASC->GetNumericAttribute(URevolverAttributeSet::GetMaxManaAttribute());
+			if (ManaBar && ManaText)
+			{
+				ManaBar->SetPercent(Mana/MaxMana); 
+				ManaText->SetText(FText::AsNumber(FMath::TruncToInt(Mana))); 
+			}
+		
+			ASC->GetGameplayAttributeValueChangeDelegate(URevolverAttributeSet::GetManaAttribute()).AddUObject(this, & URevolverPlayerMana::UpdateMana); 
+		}		
+	}
+	
+}
+
+void URevolverPlayerMana::NativeDestruct()
+{
+	Super::NativeDestruct();
+}
+
+void URevolverPlayerMana::UpdateMana(const FOnAttributeChangeData& Data)
+{
+	Mana = Data.NewValue; 
+	ManaBar->SetPercent(Mana/MaxMana);
+	ManaText->SetText(FText::AsNumber(FMath::TruncToInt(Mana)));
+}
---
+++ Source/Revolver/Public/UI/RevolverPlayerMana.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "GameplayEffectTypes.h"
+#include "RevolverPlayerMana.generated.h"
+
+class UTextBlock;
+class UProgressBar;
+/**
+ * 
+ */
+UCLASS()
+class REVOLVER_API URevolverPlayerMana : public UUserWidget
+{
+	GENERATED_UCLASS_BODY()
+public: 
+	
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+
+protected: 
+	
+	void UpdateMana(const FOnAttributeChangeData& Data);
+
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Widget", meta = (AllowPrivateAccess, BindWidgetOptional))
+	TObjectPtr<UProgressBar> ManaBar; 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Widget", meta = (AllowPrivateAccess, BindWidgetOptional))
+	TObjectPtr<UTextBlock> ManaText; 	
+	
+	float Mana; 
+	float MaxMana; 
+};
