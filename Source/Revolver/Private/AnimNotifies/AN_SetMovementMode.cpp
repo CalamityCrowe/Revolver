4140638a7d5f66793abe356e1e79913ae8feebe1
Kieran T <kieran123416@gmail.com>
2026-06-11 22:37:09 +0100

    begining the second ability for the serath enemy

---
+++ Source/Revolver/Private/AnimNotifies/AN_SetMovementMode.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimNotifies/AN_SetMovementMode.h"
+
+#include "Characters/CharacterBase.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+UAN_SetMovementMode::UAN_SetMovementMode()
+{
+}
+
+void UAN_SetMovementMode::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+	if (ACharacterBase* Character = Cast<ACharacterBase>(MeshComp->GetOwner()))
+	{
+		if (UCharacterMovementComponent* MovementComponent = Character->GetCharacterMovement())
+		{
+			MovementComponent->SetMovementMode(MovementMode); 
+		}
+	}
+}
+
+FString UAN_SetMovementMode::GetNotifyName_Implementation() const
+{
+	return Super::GetNotifyName_Implementation();
+}
---
+++ Source/Revolver/Public/AnimNotifies/AN_SetMovementMode.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AN_SetMovementMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REVOLVER_API UAN_SetMovementMode : public UAnimNotify
+{
+	GENERATED_BODY()
+public: 
+	
+	UAN_SetMovementMode(); 
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement Mode")
+	TEnumAsByte<EMovementMode> MovementMode;
+	
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+	
+	virtual FString GetNotifyName_Implementation() const override;
+	
+};
